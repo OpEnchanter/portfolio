923b959a44b0154c04aa6d3a1d6a42db14ad1a2a
OpEnchanter <openchanter@gmail.com>
2025-06-02 18:44:48 -0400

    New payload and music

--- payload.bat
+++ payload.bat
@@ -7,12 +7,12 @@ mkdir "%TEMP%\rickroll" 2>nul
 cd /d "%TEMP%\rickroll"
 
 :: Download the video using PowerShell
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/PYer-Sam/rickroll/raw/master/rickroll.mp4', 'rickroll.mp4')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://tgreenhagen.com/music.mp4', 'rickroll.mp4')"
 
 :: Check if the download was successful
 if not exist "rickroll.mp4" (
     echo Download failed. Trying alternative source...
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://archive.org/download/rick-astley-never-gonna-give-you-up/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up.mp4', 'rickroll.mp4')"
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://tgreenhagen.com/music.mp4', 'rickroll.mp4')"
 )
 
 :: Play the video in the background using Windows Media Player
