4c4527e90e0644e88afbcf949d870a8ca582cadf
Tristan Greenhagen <openchanter@gmail.com>
2025-12-13 09:52:34 -0500

    backup

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 		res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/api/getLayout", (req, res) => {
+app.get("/api/layout", (req, res) => {
 	res.sendFile(path.join(__dirname, "config", "layout.json"));
 });
 
