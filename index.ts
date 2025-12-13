82ff777ead7a2b1a786eceeb333c0e9bd131eff5
Tristan Greenhagen <openchanter@gmail.com>
2025-12-13 08:49:04 -0500

    update to host website via bun

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const port = 8080;
+
+const app = express();
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+		res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api/getLayout", (req, res) => {
+	res.sendFile(path.join(__dirname, "config", "layout.json"));
+});
+
+const server = app.listen(port, () => {
+	console.log(`App running on port ${port}`);
+});
