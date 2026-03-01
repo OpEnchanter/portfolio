75738e01f491069d684ecc47cf6d120e367b9049
Tristan Greenhagen <openchanter@gmail.com>
2026-02-28 23:07:53 -0500

    fix issues on firefox, fix ui issues & add server uptime

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import os from "node:os";
 
 const port = 8042;
 
@@ -8,6 +9,20 @@ app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
 		res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api/uptime", (req, res) => {
+    const uptimeSeconds = os.uptime() % 60;
+    const uptimeMinutes = Math.round((os.uptime() / 60)) % 60;
+    const uptimeHours = Math.round(((os.uptime() / 60) / 60)) % 24;
+    const uptimeDays = Math.round((((os.uptime() / 60) / 60) / 24));
+
+    res.send(JSON.stringify({
+        sec: uptimeSeconds,
+        min: uptimeMinutes,
+        hrs: uptimeHours,
+        day: uptimeDays
+    }));
 });
 
 app.get("/api/layout", (req, res) => {
